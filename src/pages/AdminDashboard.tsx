66215e8f2ff96a9ec8dab0b5e195effb42c88853
Aakash <162045278+aakash0101x@users.noreply.github.com>
2025-04-07 11:15:46 +0530

    Update AdminDashboard.tsx

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchAllAdmins, deleteAdminById } from '../api/api';
 
 const AdminDashboard = () => {
